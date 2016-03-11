d9fb50d51ed10b2352ad2b1f99598fdad570c68f
Oscar <OscarBellon96@gmail.com>
2016-03-11 17:47:44 +0100

    Basic template working

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'index.html', {})
+
+def post_detail(request):
+    return HttpResponse("<h1>List detail</h1>")
+
+def post_create(request):
+    return HttpResponse("<h1>Create</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
