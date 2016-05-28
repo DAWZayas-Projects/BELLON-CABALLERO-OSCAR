48a4d67b12dd1c2b135d1d76f984cfa41840df49
Oscar Bellón <oscarbellon96@hotmail.com>
2016-05-28 09:34:41 +0000

    Posts drafts and publish date

--- posts/models.py
+++ posts/models.py
@@ -28,6 +28,8 @@ class Post(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
 
     def __unicode__(self):
         return self.title
